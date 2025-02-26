83061cffd333518da5645ab3e31b92b944a3a6eb
alessandroschiano98 <schianoski98@gmail.com>
2025-02-26 12:13:50 +0100

    esercizio da completare

---
+++ paridispari/js/script.js
@@ -0,0 +1,15 @@
+// L'UTENTE DEVE SCEGLIERE FRA PARI O DISPARI // 
+
+const sceltaUserPariDispari = prompt ("Scegli pari o dispari");
+console.log(sceltaUserPariDispari);
+alert(`Hai scelto ${sceltaUserPariDispari}`);
+
+let sceltaUserNumero = prompt ("Scegli un numero da 1 a 6");
+console.log(sceltaUserNumero);
+alert (`Hai scelto ${sceltaUserNumero}`);
+
+
+
+
+
+

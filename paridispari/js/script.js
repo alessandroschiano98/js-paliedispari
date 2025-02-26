3d0a061b04e171528f7c617acb8bbb08d3e4ea1a
alessandroschiano98 <schianoski98@gmail.com>
2025-02-26 14:21:34 +0100

    numero da 1 a 6 random del computer (fatto)

--- paridispari/js/script.js
+++ paridispari/js/script.js
@@ -2,18 +2,28 @@
 
 const sceltaUserPariDispari = prompt ("Scegli pari o dispari");
 console.log(sceltaUserPariDispari);
-alert(`Hai scelto ${sceltaUserPariDispari}`);
+
 
 // L'UTENTE DEVE SCEGLIERE UN NUMERO COMPRESO TRA 1 E 6 //
 
 const sceltaUserNumero = prompt ("Scegli un numero da 1 a 6");
 console.log(sceltaUserNumero);
-alert (`Hai scelto ${sceltaUserNumero}`);
+
 
 // IL COMPUTER DEVE PRODURRE UN NUMERO RANDOM COMPRESO TRA 1 E 6 //
 
-const randomNumber = getRndInteger(1, 6);
-console.log(randomNumber);
+function randomInteger(min, max) {
+    let randomNumb= min + Math.random() * (max + 1 - min);
+    return Math.floor(randomNumb);
+  }
+  alert( randomInteger(1, 6) );
+
+
+
+
+
+  
+
 
 
 

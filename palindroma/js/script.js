ee67e263f1d866223d229677f6f347921e72076c
alessandroschiano98 <schianoski98@gmail.com>
2025-02-26 12:00:34 +0100

    esercizio eseguito (palindromo)

---
+++ palindroma/js/script.js
@@ -0,0 +1,37 @@
+
+// CHIEDERE ALL'UTENTE DI SCRIVERE UNA PAROLA // 
+const userParola = prompt ("Inserisci una parola a tuo piacimento");
+console.log(userParola);
+
+
+// Creare una funzione per capire se la parola inserita è palindroma //
+let userParolaDivisa = userParola.split('');
+console.log(userParolaDivisa); 
+
+userParolaDivisa = userParolaDivisa.reverse('');
+console.log(userParolaDivisa);
+
+const userParolaInvertita = userParolaDivisa.join('');
+console.log(userParolaInvertita);
+
+if (userParola === userParolaInvertita){
+    console.log(`La parola è palindroma`);
+} else {
+    console.log(`la parola non è palindroma`);
+}
+
+
+
+
+
+
+/* function greeting (userParola) {
+    return `${userParola} non è una parola palindroma`;
+}
+*/
+
+
+/* const number = getRndInteger (1, 6);
+console.log(number); */
+
+
